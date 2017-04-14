84e5f9e9f1a64350f3525c22215719f981fc2fc4
Michael Scott <themichaelscott@gmail.com>
2017-04-14 08:28:18 -0600

    Checks to make sure pedal faster is the default launcher before blocking windows.

--- app/src/main/java/com/pedalfaster/launcher/receiver/PedalFasterController.kt
+++ app/src/main/java/com/pedalfaster/launcher/receiver/PedalFasterController.kt
@@ -2,8 +2,10 @@ package com.pedalfaster.launcher.receiver
 
 import android.app.Application
 import android.content.Context.WINDOW_SERVICE
+import android.content.Intent
 import android.graphics.PixelFormat
 import android.view.WindowManager
+import com.pedalfaster.launcher.R
 import com.pedalfaster.launcher.event.CheckBluetoothStatusEvent
 import com.pedalfaster.launcher.prefs.Prefs
 import com.pedalfaster.launcher.view.PedalFasterView
@@ -44,6 +46,8 @@ class PedalFasterController
     @Synchronized
     fun notifyOfBluetoothStatus() {
         when {
+            !isPedalFasterTheDefaultLauncher() -> dismissPedalFasterView()
+            !prefs.keepPedalingEnabled -> dismissPedalFasterView() // if keep pedaling is disabled, then make sure to dismiss any windows
             getActiveDeviceStatus() == BluetoothStatus.CONNECTED -> dismissPedalFasterView()
             showAlert -> showPedalFasterView()
         }
@@ -75,5 +79,19 @@ class PedalFasterController
         val bluetoothStatus = bluetoothStatusMap[prefs.activeBluetoothDeviceAddress] ?: BluetoothStatus.UNKNOWN
         Timber.d("Bluetooth status for ${prefs.activeBluetoothDeviceAddress}: $bluetoothStatus")
         return bluetoothStatus
+    }
+
+    private fun isPedalFasterTheDefaultLauncher(): Boolean {
+        val packageManager = application.packageManager
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+
+        val info = packageManager.resolveActivity(intent, 0)
+        val applicationLabel = packageManager.getApplicationLabel(info.activityInfo.applicationInfo)
+        Timber.d("Current default launcher - $applicationLabel")
+        val pedalFasterActive = applicationLabel == application.getString(R.string.app_name)
+        Timber.d("Pedal faster is active - $pedalFasterActive")
+        return pedalFasterActive
     }
 }
